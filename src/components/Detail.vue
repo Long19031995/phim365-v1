9bfdbbb42e99479430e7bece0980629991b9dbed
Kim Long <longnguyen@beeketing.net>
2020-02-04 21:23:03 +0700

    commit

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -19,16 +19,23 @@
                 <h3 class="mb24">
                     {{ detail.nameEnglish }}
                 </h3>
-                <!-- <div v-for="(server, index) in detail.server" :key="index" class="flex align-center mb16">
-                    <div class="mr16">
-                        {{ server.name }}
+
+                <hr class="bg-n600">
+                <div class="pt16">
+                    <h2 class="pb8">
+                        Server
+                    </h2>
+                    <div v-for="(server, index) in detail.server" :key="index" class="flex align-center mb16">
+                        <div class="mr16">
+                            {{ server.name }}
+                        </div>
+                        <div class="flex">
+                            <a :href="chapter.links[0].source" target="_blank" v-for="(chapter, index) in server.chapters" :key="index" class="text-decoration-none pr12 pl12 mr4 cl-white bg-g200 br4">
+                                {{ chapter.name }}
+                            </a>
+                        </div>
                     </div>
-                    <div class="flex">
-                        <a :href="chapter.links[0].source" target="_blank" v-for="(chapter, index) in server.chapters" :key="index" class="text-decoration-none p4 mr4 cl-white bg-g200 br4">
-                            {{ chapter.name }}
-                        </a>
-                    </div>
-                </div> -->
+                </div>
 
                 <hr class="bg-n600">
                 <div class="pt16 pb16">
